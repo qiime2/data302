52ef291038f08a5fb0dc58517fe4606466f9507f
Liz Gehret <elizabeth.gehret@gmail.com>
2024-06-24 14:45:05 -0700

    MAINT: moves classifiers to static (non-release) URLs

--- urls/_classifiers.py
+++ urls/_classifiers.py
@@ -9,6 +9,8 @@
 # flake8: noqa
 
 MAP_CLASSIFIERS = {
+    # sklearn 0.24.1
+
     # Greengenes
     'classifiers/greengenes/gg_12_10_otus.tar.gz':
         'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/greengenes/gg_12_10_otus.tar.gz',
@@ -19,5 +21,23 @@ MAP_CLASSIFIERS = {
     'classifiers/greengenes/gg_2022_10_backbone_full_length.nb.qza':
         'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/greengenes/gg_2022_10_backbone_full_length.nb.qza',
     'classifiers/greengenes/gg_2022_10_backbone.v4.nb.qza':
-        'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/greengenes/gg_2022_10_backbone.v4.nb.qza'
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/greengenes/gg_2022_10_backbone.v4.nb.qza',
+
+    # sklearn 1.4.2
+
+    # Greengenes 2
+    'classifiers/sklearn-1.4.2/greengenes2/2022.10.backbone.full-length.nb.sklearn-1.4.2.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/sklearn-1.4.2/greengenes2/2022.10.backbone.full-length.nb.sklearn-1.4.2.qza',
+    'classifiers/sklearn-1.4.2/greengenes2/2022.10.backbone.v4.nb.sklearn-1.4.2.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/sklearn-1.4.2/greengenes2/2022.10.backbone.v4.nb.sklearn-1.4.2.qza',
+    # GTDB
+    'classifiers/sklearn-1.4.2/gtdb/gtdb_classifier_r220.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/sklearn-1.4.2/gtdb/gtdb_classifier_r220.qza',
+    'classifiers/sklearn-1.4.2/gtdb/gtdb_human_stool_weighted_classifier_r220.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/sklearn-1.4.2/gtdb/gtdb_human_stool_weighted_classifier_r220.qza',
+    # Silva
+    'classifiers/sklearn-1.4.2/silva/silva-138-99-nb-classifier.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/sklearn-1.4.2/silva/silva-138-99-nb-classifier.qza',
+    'classifiers/sklearn-1.4.2/silva/silva-138-99-nb-human-stool-weighted-classifier.qza':
+        'https://s3-us-west-2.amazonaws.com/qiime2-data/classifiers/sklearn-1.4.2/silva/silva-138-99-nb-human-stool-weighted-classifier.qza',
 }
